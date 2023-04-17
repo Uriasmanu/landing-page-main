52630fde18e00dc48d6725ab47ed5b50d200652c
Manoela Urias <maanoelaurias@gmail.com~>
2023-04-16 21:23:45 -0300

    inicio da versão mobile

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+function trocarEstilo() {
+  var largura = window.innerWidth;
+  var estilo = document.getElementById("estilo");
+
+  if (largura < 500) {
+    estilo.href = "mobile.css";
+  } else {
+    estilo.href = "style.css";
+  }
+
+  if (largura < 500 && estilo.href.includes("mobile.css")) {
+    estilo.disabled = true;
+  } else {
+    estilo.disabled = false;
+  }
+}
+
+window.addEventListener("load", trocarEstilo);
+window.addEventListener("resize", trocarEstilo);
+
+
+    // verifica o tamanho da tela no início do carregamento da página
+    window.dispatchEvent(new Event('resize'));
 var dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(function (dropdown) {
   dropdown.addEventListener('mouseover', function () {
